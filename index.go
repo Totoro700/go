9634b7105bd96a946691e9682dba4c525a0da265
Totoro700 <Qwerty82994@gmail.com>
2020-12-10 12:33:05 -0600

    Update index.go

--- index.go
+++ index.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-    "fmt"
+    "fmt -s"
 )
 
 func main(){
     fmt.Println("Hello!")
+    fmt.Println("What is 1 * 1?")
+    var input;
+    fmt.Scanline(&input)
+    if (input == "1"){
+		fmt.Println("Correct!")
+	}else {
+		fmt.Println("Incorrect!")
+	}
 }
